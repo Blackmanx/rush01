df2da07f61a2ee2ff8ea3acf95356d49b8c5bdeb
Blackmanx <blackman@sajl.tk>
2019-10-13 22:24:06 +0200

    Corrected syntax, added ft_print_arr, added variables
    	modified:   ex00/rush-01.c
    	deleted:    es.subject.pdf

--- ex00/rush-01.c
+++ ex00/rush-01.c
@@ -18,7 +18,9 @@ int	ft_strlen(char *str);
 char	ft_rush(char *str)
 {
 	int permutations [24][4];
-	
+	int n = 0;
+	int per_selected;
+
 	permutations = {
 	{1,2,3,4}, {1,2,4,3}, {1,3,2,4}, {1,3,4,2}, {1,4,2,3}, {1,4,3,2},
 	{2,1,3,4}, {2,1,4,3}, {2,3,1,4}, {2,3,4,1}, {2,4,1,3}, {2,4,3,1},
@@ -26,7 +28,12 @@ char	ft_rush(char *str)
 	{4,1,2,3}, {4,1,3,2}, {4,2,1,3}, {4,2,3,1}, {4,3,1,2}, {4,3,2,1}
 	};
 
-
+	if (str[n] % 2 == 0)
+	{
+		//ToDo
+		ft_print_arr(str[per_selected]);
+	}
+}
 
 
 int	main(int argc, char *argv[])
@@ -34,7 +41,7 @@ int	main(int argc, char *argv[])
 	char *buffer;
 
 	buffer = malloc(ft_strlen(argv[1])+1);
-	if (argc != 2 || ft_checkargv(argv[1]) == 1 || ft_strlen(argv[1] != 30)
+	if (argc != 2 || ft_checkargv(argv[1]) == 1 || ft_strlen(argv[1] != 30))
 	{
 		write(1, "Error\n", 6);
 		free(buffer);
@@ -57,4 +64,18 @@ int	ft_checkargv(char *str)
 		return (1);
 	}
 	return (0);
+}
+
+void	ft_print_arr(int *arr)
+{
+	int n;
+
+	n = 0;
+	while (arr[n] != '\0')
+	{
+		write(1, &arr[n], 1);
+		write(1, " ", 1);
+		n++;
+	}
+	write(1, "\n", 1);
 }
